bb4dfde801c95e9dc8d84a95bcf5f043eca93648
KUDIRET <ekuduret@mail.ru>
2022-11-30 19:14:23 +0600

    Version 1

---
+++ ICT.java
@@ -0,0 +1,11 @@
+public class ICT {
+    public static void main(String[] args){
+        System.out.println("Name: Kudiret");
+        System.out.println("Surname: Eszhanov");
+        System.out.println("ID: 220107090");
+        System.out.println("Hobby: Sport programing,Watch YouTube");
+        System.out.println("How much do you like ICT(fron 1-10)): 7");
+        System.out.println("Rate this task from 1 to 10: 10/10 ");
+        System.out.println("Am I the best teacher?: Absolutely yes");
+    }
+}
